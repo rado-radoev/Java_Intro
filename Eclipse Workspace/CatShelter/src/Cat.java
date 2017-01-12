45336ae5d393214e9a222240e15e79f0c98f2c5d
superklamer <rradoev@gmail.com>
2017-01-11 17:48:42 -0800

    Finish up from last night

---
+++ Eclipse Workspace/CatShelter/src/Cat.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Radoslav Radoev
+ *
+ */
+public class Cat {
+	/**
+	 * Cat name attribute
+	 */
+	String name = null;
+	
+	
+	/**
+	 * Create Cat object
+	 * @param name	The name of the cat
+	 */
+	public Cat (String name) {
+		this.name = name;
+	}
+
+}
