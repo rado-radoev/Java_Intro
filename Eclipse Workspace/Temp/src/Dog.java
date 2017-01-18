4111a0e51c265fdbf11e29fea8745a6065785992
superklamer <rradoev@gmail.com>
2017-01-17 18:29:32 -0800

    Constructor overloading examples

---
+++ Eclipse Workspace/Temp/src/Dog.java
@@ -0,0 +1,23 @@
+
+public class Dog {
+	
+	public Dog () {
+		this("Sharo");
+	}
+	
+	public Dog (String name) {
+		this(name, 5);
+	}
+	
+	public Dog (String name, int age) {
+		this(name, age, 0.4);
+	}
+	
+	public Dog (String name, int age, double length) {
+		this(name, age, length, "Bliack");
+	}
+	
+	public Dog(String name, int age, double length, String collar) {
+		
+	}
+}
---
+++ Eclipse Workspace/Temp/src/DogMain.java
@@ -0,0 +1,11 @@
+
+public class DogMain {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog("Petko", 5, 55);
+		
+		syso
+
+	}
+
+}
