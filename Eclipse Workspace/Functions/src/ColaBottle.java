ea73d6edfa3576f5589d6a767530a28852bc7f73
superklamer <rradoev@gmail.com>
2017-01-19 22:39:35 -0800

    Assignment #4

---
+++ Eclipse Workspace/Functions/src/ColaBottle.java
@@ -0,0 +1,13 @@
+
+public class ColaBottle {
+
+	private static final double price = 2.99;
+	
+	public ColaBottle () {}
+	
+	// Get cola bottle price
+	public double getColaPrice () {
+		return price;
+	}
+
+}
