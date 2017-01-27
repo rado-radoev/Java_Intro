1ccad3529d7f7064e62ba36111039ec447f24d24
superklamer <rradoev@gmail.com>
2017-01-26 20:44:55 -0800

    Binary search

--- Eclipse-dev/BinarySearch/src/BinarySearch.java
+++ Eclipse-dev/BinarySearch/src/BinarySearch.java
@@ -3,17 +3,27 @@ public class BinarySearch {
     public static void main(String[] args) {
       /**Binary search algorithm - search value x = 75*/
       int[] numbers = {15,23,26,41,52,59,75,84,91};
+    
+      int num = 75;
+      int i = 1;
+      int j = numbers.length;
+      
+      while (i < j) {
+    	  int m = (i + j) / 2;
+    	  
+    	  if (num > numbers[m]) {
+    		  i = m + 1;
+    	  }
+    	  else {
+    		  j = m;
+    	  }
+      }
+      
+      if (num == numbers[i]) {
+    	  System.out.println(num);
+      }
+      else {
+    	  System.out.println("fuck");
+      }
     }
-
-    /**
-        Get the 1st and last index of the array
-        Dived it by two to find the middle index
-        Check if middle index is equal to the number we are looking for
-          if yes break;
-          esle
-        Check if x is higher or lower than the middle index
-        if higher move lower index to the middle +1
-        if lower move the higher index to the middle - 1
-
-    */
 }
---
+++ Eclipse-dev/BinarySearch/src/CompareInts.java
@@ -0,0 +1,14 @@
+
+public class CompareInts {
+	
+	public static void main(String[] args) 
+	{
+		int five = 5;
+		int six = 5;
+		
+		if (five == six)
+			System.out.println("printed");
+	}
+
+		
+}
