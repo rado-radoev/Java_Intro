fc1528b7a25c0ddeeeaaa52fd7553643fa2184b7
Radoslav Radoev <rradoev@gmail.com>
2017-02-08 16:31:48 -0800

    Junk class

---
+++ Eclipse-dev/Temp/src/Junk.java
@@ -0,0 +1,21 @@
+
+public class Junk {
+	
+	public static void main(String[] args) {
+
+	}
+	
+	public static boolean Test() {
+		String a = "1";
+		String b = "2";
+		
+		int c = 1;
+		int d = 10;
+		
+		while (c < d) c++;
+		
+		if (a.equals(b)) return true;
+		else return false;	
+	}
+
+}
