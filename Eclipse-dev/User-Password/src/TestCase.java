33455d113e78c8ae09ce9221aa08c4d08968053b
superklamer <rradoev@gmail.com>
2017-02-20 21:42:15 -0800

    Added users hashmap to testcase

--- Eclipse-dev/User-Password/src/TestCase.java
+++ Eclipse-dev/User-Password/src/TestCase.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Set;
 
 public class TestCase {
 
@@ -14,6 +16,14 @@ public class TestCase {
 		System.out.println("The username is: " + user2.getUsername() + ". " +
 				"The role is: " + user2.getRole() + ". " + 
 				"The password is: " + user2.getPassword());
+		
+		
+		HashMap<Users, Password> userMap = new HashMap<Users, Password>();
+		userMap.put(key, value);
+		Set<Users> userKey = userMap.keySet();
+		for (Users u : userKey) {
+			System.out.println();
+		}
 
 	}
 
