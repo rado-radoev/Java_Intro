76743b56d64ec61c367e2e4ecd6f78497a54255a
superklamer <rradoev@gmail.com>
2017-02-21 20:37:58 -0800

    User Password completed!

--- Eclipse-dev/User-Password/src/TestCase.java
+++ Eclipse-dev/User-Password/src/TestCase.java
@@ -1,32 +1,118 @@
+import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Scanner;
 
 public class TestCase {
+	public static HashMap<User, Password> userMap = new HashMap<User, Password>();
+	
+	public static void main(String[] args) throws AccessDeniedException {
 
-	public static void main(String[] args) {
-		Password pass = new Password("fuckingPassword");
-		System.out.println();
+		// Instantiating default password to use
+		Password pass = new Password();
 		
-		Users user = new Users("Petko", "Manager", "Huiovina2");
-//		System.out.println("The username is: " + user.getUsername() + ". " +
-//				"The role is: " + user.getRole() + ". " + 
-//				"The password is: " + user.getPassword());
+		// Instantiating user objects
+		User user = new User("Bob", "user");
+		User user2 = new User("Jim", "user");
+		User user3 = new User("Liz", "super user");
 		
-		Users user2 = new Users("Vladko", "Po-golemo Shefche", "BashHui@2");
-//		System.out.println("The username is: " + user2.getUsername() + ". " +
-//				"The role is: " + user2.getRole() + ". " + 
-//				"The password is: " + user2.getPassword());
+		// Adding users to map
+		userMap.put(user, pass);
+		userMap.put(user2, new Password("SuperMegaPassw0rd"));
+		userMap.put(user3, new Password("JavaIsTheBest1"));
 		
-		
-		HashMap<Users, String> userMap = new HashMap<Users, String>();
-		userMap.put(user, user.getPassword());
-		userMap.put(user2, user2.getPassword());
-		
-		Set<Users> userKey = userMap.keySet();
-		for (Users u : userKey) {
-			System.out.println(u.getUsername() + " " + u.getPassword());
+		// Ask the user for username and password and verifies if they are correct
+		while (true) {
+			System.out.println("Enter user name:");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.next();
+			
+			// Output username and password to Array
+			String[] userPass = FindUserAndPassword(input);
+			
+			// Verify if the username is in the array on position 0. 
+			// If not ask again, else fall through
+			if (!VerifyUserExists(userPass)) {
+				System.out.println("User does not exist");
+				continue;
+			}
+	
+			// Test if the password is correct.
+			// If password match output to screen and exit
+			// Else throw an exception and display Access Denied message.
+			try  {
+				System.out.println("Enter password:");
+				input = scanner.next();
+				if (VerifyPassword(userPass, input)) {
+					System.out.println("User: " + userPass[0] + "\n" +
+							"Password: " + userPass[1] + "\n" + 
+							"Welcome, " + userPass[0] + "!");
+				}
+				break;
+				
+			} 
+			catch (AccessDeniedException e) {
+				System.out.println("Access Denied!");
+				break;
+			}
 		}
-
 	}
+	
+	/**
+	 * Check if the user specified exists in the HashMap
+	 * @param input		input as Array. username is string and is first element in Array
+	 * @return			true if user exists. false if user does not exist.
+	 */
+	public static boolean VerifyUserExists(String[] input)  {
+		if (input[0] == null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Verifies if the user password is correct
+	 * @param input		input as Array. Password is string and is second element in Array.
+	 * @param pass		password as String. Entered by user.
+	 * @return			true if passwords match
+	 * @throws AccessDeniedException	if passwords do not match an exception is thrown.
+	 */
+	public static boolean VerifyPassword(String[] input, String pass) throws AccessDeniedException {
+		if (input[1].equals(pass)) {
+			return true;
+		}
+		else {
+			throw new AccessDeniedException(null);
+		}
+	}
+	
 
+	/**
+	 * Method to check if user exists in the HashMap
+	 * @param user		username as String from user input
+	 * @return			array of username and password as string
+	 */
+	public static String[] FindUserAndPassword(String user) {
+		String[] userAndPass = new String[2];
+		
+		// Iterate through the map using Entry Set
+		Iterator<Entry<User, Password>> entrySetIterator = userMap.entrySet().iterator();
+		
+		while (entrySetIterator.hasNext()) {
+			Entry entry = entrySetIterator.next();
+			
+			// Type casting needed for the User and Password objects
+			User u = (User)entry.getKey();
+			Password p = (Password)entry.getValue();
+			
+			if (user.toLowerCase().equals(u.getUsername().toLowerCase())) {
+				userAndPass[0] = u.getUsername();
+				userAndPass[1] = p.getPassword();
+			}
+		}
+		return userAndPass;
+	}
 }
