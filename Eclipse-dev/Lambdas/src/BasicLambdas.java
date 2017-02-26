252790aec5c51bdf6ad16ac3171906e0e195533f
superklamer <rradoev@gmail.com>
2017-02-26 00:38:32 -0800

    lambda

--- Eclipse-dev/Lambdas/src/BasicLambdas.java
+++ Eclipse-dev/Lambdas/src/BasicLambdas.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.*;
 
@@ -9,9 +10,22 @@ public class BasicLambdas {
 
 	public static void main(String[] args) {
 
-		List<String> list = Arrays.asList("Rado", "Viki", "Stefcho", "Valya", "Yotko", "Ogi");
-		list.sort(String::compareTo);
+		String[] names = {"Rado", "Viki", "Stefcho", "Valya", "Yotko", "Ogi"};
+		List<String> family = Arrays.asList(names);
+		
+		Arrays.sort(names, new Comparator<String>() {
 
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.length() - o2.length();
+			}
+			
+		});
+		
+		Comparator<String> sortByLenght = (String s1, String s2) -> s1.length() - s2.length();
+		Arrays.sort(names, sortByLenght);
+		
+		
 	}
 	
 	
