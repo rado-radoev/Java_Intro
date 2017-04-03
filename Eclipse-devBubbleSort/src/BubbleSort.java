969df2e487e69b9609c702a7f74922dca6a141b4
Radoslav Radoev <rradoev@gmail.com>
2017-04-03 14:10:24 -0700

    bubble sort

--- Eclipse-devBubbleSort/src/BubbleSort.java
+++ Eclipse-devBubbleSort/src/BubbleSort.java
@@ -2,8 +2,30 @@
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] arr = {5,1,4,2,8};
+		bubbleSort(arr, arr.length);
 
 	}
 
+	private static int[] bubbleSort (int[] list, int lenght) {
+		boolean swap = true;
+		int temp;
+		
+		while (swap) {
+			swap = false;
+			
+			for(int i = 0; i < list.length - 1;i++) {
+				if (list[i] > list[i + 1]) {
+					temp = list[i];
+					list[i] = list[i + 1];
+					list[i + 1] = temp;
+					swap = true;
+				}
+			}
+		}
+		
+		return list;
+	}
+	
 }
